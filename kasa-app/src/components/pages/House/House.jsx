e3882eef5fc087a55e921168b3fcd59f5a3862fd
stephane.malho <stepahen.malho@gmail.com>
2023-08-23 18:33:49 +0200

    ✨ House > first settup to add data with house id

--- kasa-app/src/components/pages/House/House.jsx
+++ kasa-app/src/components/pages/House/House.jsx
@@ -2,15 +2,57 @@ import React from "react";
 import Header from "../../layout/header/Header";
 import Footer from "../../layout/footer/Footer";
 import { useParams } from "react-router-dom";
+import housings from "../../../data/housing";
 
 const House = () => {
-
   const { id } = useParams();
-
+  const house = housings.find((house) => house.id === id);
+  console.log(id);
+  console.log(house);
   return (
     <>
       <Header />
-      <h1>House ${id}</h1>
+      <section className="house-section">
+        <div className="image-container">
+          <img src={house.pictures[0]} alt={house.title} />
+        </div>
+        <div className="house-content">
+          <div className="head-house-content">
+          <div className="house-content__title">
+            <h2>{house.title}</h2>
+            <p>{house.location}</p>
+          </div>
+          <div className="house-content__host">
+            <div className="house-content__host__name">
+              <p>{house.host.name}</p>
+            </div>
+            <div className="house-content__host__avatar">
+              <img src={house.host.picture} alt={house.host.name} />
+            </div>
+          </div>
+          </div>
+          <div className="house-content__tags">
+            <ul>
+              {house.tags.map((tag) => (
+                <li key={tag}>{tag}</li>
+              ))}
+            </ul>
+          </div>
+          <div className="house-content__rating">
+            <p>{house.rating}</p>
+          </div>
+          <div className="house-content__description">
+            <p>{house.description}</p>
+          </div>
+          <div className="house-content__equipments">
+            <ul>
+              {house.equipments.map((equipment) => (
+                <li key={equipment}>{equipment}</li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </section>
       <Footer />
     </>
   );
