f4d85b49a7cf539d90cf43f6efbeda93dbfce884
stephane.malho <stepahen.malho@gmail.com>
2023-09-04 15:55:38 +0200

    ✨ House > create carrousel

--- kasa-app/src/components/pages/House/House.jsx
+++ kasa-app/src/components/pages/House/House.jsx
@@ -1,14 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "../../layout/header/Header";
 import Footer from "../../layout/footer/Footer";
 import { useParams } from "react-router-dom";
 import housings from "../../../data/housing";
 import Collapse from "../../layout/main/house/Collapse";
 import OwnerInfo from "../../layout/main/house/OwnerInfo";
+import { AiOutlineLeft, AiOutlineRight } from "react-icons/ai";
 
 const House = () => {
   const { id } = useParams();
   const house = housings.find((house) => house.id === id);
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+  const nextImage = () => {
+    if (currentImageIndex === house.pictures.length - 1) {
+      setCurrentImageIndex(0);
+    } else {
+      setCurrentImageIndex(currentImageIndex + 1);
+    }
+  };
+  
+  const prevImage = () => {
+    if (currentImageIndex === 0) {
+      setCurrentImageIndex(house.pictures.length - 1);
+    } else {
+      setCurrentImageIndex(currentImageIndex - 1);
+    }
+  };
 
   console.log(id);
   console.log(house);
@@ -17,13 +35,19 @@ const House = () => {
       <Header />
       <section className="house-section">
         <div className="image-container">
-          <img src={house.pictures[0]} alt={house.title} />
+          <AiOutlineLeft size={60} onClick={prevImage} className="carousel-icon-left" />
+          <img src={house.pictures[currentImageIndex]} alt={house.title} />
+          <AiOutlineRight size={60} onClick={nextImage} className="carousel-icon-right" />
         </div>
         <div className="house-content">
           <OwnerInfo />
           <div className="collapses-content">
             <Collapse title="Description" houseInfo={house.description} />
-            <Collapse title="Equipements" key={house.id} houseList={house.equipments}  />
+            <Collapse
+              title="Equipements"
+              key={house.id}
+              houseList={house.equipments}
+            />
           </div>
         </div>
       </section>
