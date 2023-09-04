780ea2d3d3d7b413145135bf8ab97008a2220f7b
stephane.malho <stepahen.malho@gmail.com>
2023-09-04 10:03:54 +0200

    ✨ House > add layout for house global description

--- kasa-app/src/components/pages/House/House.jsx
+++ kasa-app/src/components/pages/House/House.jsx
@@ -18,30 +18,33 @@ const House = () => {
         </div>
         <div className="house-content">
           <div className="head-house-content">
-          <div className="house-content__title">
-            <h2>{house.title}</h2>
-            <p>{house.location}</p>
-          </div>
-          <div className="house-content__host">
-            <div className="house-content__host__name">
-              <p>{house.host.name}</p>
+            <div className="house-content__title">
+              <h2>{house.title}</h2>
+              <p>{house.location}</p>
+              <ul className="house-content__tags">
+                {house.tags.map((tag) => (
+                  <li key={tag}>{tag}</li>
+                ))}
+              </ul>
             </div>
-            <div className="house-content__host__avatar">
-              <img src={house.host.picture} alt={house.host.name} />
+            <div className="house-content__host">
+              <div className="house-content__user">
+                <div className="house-content__user-identity">
+                  <p className="house-content__host__name">{house.host.name}</p>
+                  <img
+                    className="house-content__host__avatar"
+                    src={house.host.picture}
+                    alt={house.host.name}
+                  />
+                </div>
+              </div>
+              <p className="house-content__rating">
+                {/* Le map des étoiles sear ici */}
+              </p>
             </div>
           </div>
-          </div>
-          <div className="house-content__tags">
-            <ul>
-              {house.tags.map((tag) => (
-                <li key={tag}>{tag}</li>
-              ))}
-            </ul>
-          </div>
-          <div className="house-content__rating">
-            <p>{house.rating}</p>
-          </div>
-          <div className="house-content__description">
+
+          {/* <div className="house-content__description">
             <p>{house.description}</p>
           </div>
           <div className="house-content__equipments">
@@ -50,7 +53,7 @@ const House = () => {
                 <li key={equipment}>{equipment}</li>
               ))}
             </ul>
-          </div>
+          </div> */}
         </div>
       </section>
       <Footer />
